eca9b323181cd8403f0721bff3f26278230c5c3d
Andrey Sumin <an.sumin@hh.ru>
2014-07-31 17:21:35 +0400

    HH-44472 fix package version

--- hhwebutils/packaging.py
+++ hhwebutils/packaging.py
@@ -9,6 +9,6 @@ def parse_version_from_changelog(package_name, root_dir):
         deb_path = os.path.join(root_dir, 'debian/changelog')
         with open(deb_path, 'r') as changelog:
             regmatch = re.match(r'%s \((.*)\).*' % package_name, changelog.readline())
-            return regmatch.group(0)
+            return regmatch.group(1)
     except (IOError, AttributeError):
         return 'unknown_version'
