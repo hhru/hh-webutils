ab5973be42c61d9d25573a469181ca9a48c02608
Ilya Khokhryakov <ilyahohryakov@gmail.com>
2016-01-20 18:32:48 +0300

    HH-57112 move date_intervals from xhh

---
+++ hhwebutils_tests/test_date_intervals.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+from datetime import datetime
+from common import TestCaseWithXml
+
+from hhwebutils import date_intervals
+
+
+class TestDateIntervals(TestCaseWithXml):
+
+    def test_get_date_interval(self):
+        self.assertEqual(date_intervals.get_date_interval(datetime(2010, 10, 30), datetime(2012, 10, 30)), (2, 1))
+        self.assertEqual(date_intervals.get_date_interval(datetime(2012, 10, 30), datetime(2012, 11, 30)), (0, 2))
+        self.assertEqual(date_intervals.get_date_interval(datetime(2012, 10, 30), datetime(2012, 10, 30)), (0, 1))
+        self.assertEqual(date_intervals.get_date_interval(datetime(2012, 10, 29), datetime(2012, 10, 30)), (0, 1))
+
+    def test_get_date_intervals_union(self):
+        intervals = [
+            date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 2, 1)),
+            date_intervals.DateInterval(datetime(2012, 2, 1), datetime(2012, 3, 1)),
+            date_intervals.DateInterval(datetime(2012, 3, 15), datetime(2012, 5, 15)),
+            date_intervals.DateInterval(datetime(2012, 4, 15), datetime(2012, 7, 15)),
+            date_intervals.DateInterval(datetime(2012, 8, 1), datetime(2012, 12, 1)),
+            date_intervals.DateInterval(datetime(2012, 9, 1), datetime(2012, 10, 1)),
+        ]
+
+        result = [
+            date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 3, 1)),
+            date_intervals.DateInterval(datetime(2012, 3, 15), datetime(2012, 7, 15)),
+            date_intervals.DateInterval(datetime(2012, 8, 1), datetime(2012, 12, 1)),
+        ]
+
+        self.assertEqual(date_intervals.get_date_intervals_union(intervals), result)
+
+        intervals = [date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 2, 1))]
+        result = [date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 2, 1))]
+
+        self.assertEqual(date_intervals.get_date_intervals_union(intervals), result)
+
+    def test_get_date_intervals_sum(self):
+        intervals = [
+            date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 3, 1)),
+            date_intervals.DateInterval(datetime(2012, 3, 15), datetime(2012, 7, 15)),
+            date_intervals.DateInterval(datetime(2012, 8, 1), datetime(2012, 12, 1)),
+        ]
+
+        self.assertEqual(date_intervals.get_date_intervals_sum(intervals), (1, 1))
+
+        self.assertEqual(date_intervals.get_date_intervals_sum(
+            [date_intervals.DateInterval(datetime(2012, 1, 1), datetime(2012, 3, 1))]),
+            (0, 3)
+        )
+        self.assertEqual(date_intervals.get_date_intervals_sum([]), (0, 0))
