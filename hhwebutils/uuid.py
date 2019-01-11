04c77ee2efc9f981675e4a12673ea0616a9d104a
boksh <e.bokshitskiy@hh.ru>
2019-01-11 17:17:49 +0300

    HH-87190 update regexp

--- hhwebutils/uuid.py
+++ hhwebutils/uuid.py
@@ -1,10 +1,10 @@
 import re
 
 # 01234567-89ab-cdef-0123-456789abcdef
-UUID_RE = re.compile(r'[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}', re.I)
+UUID_RE = re.compile('UUID:? ([0-9a-zA-Z-]+)')
 
 
 def parse_uuid(text):
     uuid = re.search(UUID_RE, text)
     if uuid is not None:
-        return uuid.group()
+        return uuid.group(1)
--- hhwebutils_tests/test_uuid.py
+++ hhwebutils_tests/test_uuid.py
@@ -6,24 +6,31 @@ from hhwebutils.uuid import parse_uuid
 
 
 class TestUrls(unittest.TestCase):
+    user_agents_patterns = [
+        'ApplicantHH (iPhone 6 Plus; iOS 11.1.2; Version/5.1.1812.885; UUID {}; ru.hh.iphone)',
+        'ApplicantHH (iPhone 6 Plus; iOS 11.1.2; Version/5.1.1812.885; UUID: {}; ru.hh.iphone)',
+
+        'Mozilla/5.0 (Linux; Android 8.0.0; AUM-L29 Build/HONORAUM-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36 ru.hh.android/5.2.1.387, Device: AUM-L29, Android OS: '
+        '8.0.0 (UUID: {})',
+
+        'Mozilla/5.0 (Linux; Android 8.0.0; AUM-L29 Build/HONORAUM-L29; wv) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36 ru.hh.android/5.2.1.387, Device: AUM-L29, Android OS: '
+        '8.0.0 (UUID {})'
+    ]
 
     def test_uppercase_uuid_parse(self):
         uuid_in_text = 'D7B64537-BFD1-4981-8B25-6C09E1163A3E'
-        parsed_uuid = parse_uuid(
-            'ApplicantHH (iPhone 6 Plus; iOS 11.1.2; Version/5.1.1812.885; UUID {}; ru.hh.iphone)'.format(uuid_in_text)
-        )
-        self.assertEqual(uuid_in_text, parsed_uuid)
+        for ua in self.user_agents_patterns:
+            self.assertEqual(uuid_in_text, parse_uuid(ua.format(uuid_in_text)))
 
     def test_lowercase_uuid_parse(self):
         uuid_in_text = 'd7b64537-bfd1-4981-8b25-6c09e1163a3e'
-        parsed_uuid = parse_uuid(
-            'ApplicantHH (iPhone 6 Plus; iOS 11.1.2; Version/5.1.1812.885; UUID {}; ru.hh.iphone)'.format(uuid_in_text)
-        )
-        self.assertEqual(uuid_in_text, parsed_uuid)
+        for ua in self.user_agents_patterns:
+            self.assertEqual(uuid_in_text, parse_uuid(ua.format(uuid_in_text)))
 
-    def test_no_uuid_in_text(self):
-        uuid_in_text = 'СОВСЕМ-НЕ-ПОХОЖЕ-НА-UUID'
-        parsed_uuid = parse_uuid(
-            'ApplicantHH (iPhone 6 Plus; iOS 11.1.2; Version/5.1.1812.885; UUID {}; ru.hh.iphone)'.format(uuid_in_text)
-        )
-        self.assertIsNone(parsed_uuid)
+    def test_no_uuid_value(self):
+        self.assertIsNone(parse_uuid(
+            'Mozilla/5.0 (Linux; Android 8.0.0; AUM-L29 Build/HONORAUM-L29; wv) AppleWebKit/537.36 (KHTML, '
+            'like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36'
+        ))
